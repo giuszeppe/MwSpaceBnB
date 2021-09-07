84ae854aa5dd857e0fd34886c0c48344fefef019
usermwspace <user@mwspace.com>
2021-09-07 11:34:51 +0200

    created create view for appartment

--- resources/views/appartamenti/create.blade.php
+++ resources/views/appartamenti/create.blade.php
@@ -7,39 +7,39 @@
                     <div class="form-row">
                         <div class="col">
                             <label for="title">Title</label>
-                            <input type="text" class="form-control" id="title" placeholder="Titolo dell'appartamento">
+                            <input type="text" class="form-control" id="title" placeholder="Titolo dell'appartamento" required>
                         </div>
                     </div>
                     <div class="form-row mt-4 form-number">
                         <div class="col">
                             <div class="form-group">
                                 <label for="title">Numero stanze</label>
-                                <input type="number" class="form-control" id="title" placeholder="0">
+                                <input type="number" class="form-control" id="title" placeholder="0" min=0 required>
                             </div>
                         </div>
                         <div class="col">
                             <div class="form-group">
                                 <label for="title">Numero posti letto</label>
-                                <input type="number" class="form-control" id="title" placeholder="0">
+                                <input type="number" class="form-control" id="title" placeholder="0" min=0 required>
                             </div>
                         </div>
                         <div class="col">
                             <div class="form-group">
                                 <label for="title">Numero bagni</label>
-                                <input type="number" class="form-control" id="title" placeholder="0">
+                                <input type="number" class="form-control" id="title" placeholder="0" min=0 required>
                             </div>
                         </div>
                         <div class="col">
                             <div class="form-group">
                                 <label for="title">Metri quadrati</label>
-                                <input type="number" class="form-control" id="title" placeholder="0">
+                                <input type="number" class="form-control" id="title" placeholder="0" min=0 required>
                             </div>
                         </div>
                     </div>
                     <div class="form-row">
                         <div class="form-group">
                             <label for="title">Indirizzo</label>
-                            <input type="address" class="form-control" id="title" placeholder="Indirizzo">
+                            <input type="address" class="form-control" id="title" placeholder="Indirizzo" required>
                         </div>
                     </div>
                     <div class="form-row">
