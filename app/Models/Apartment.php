ea354a4359e6dd0dd9180a2f90676e22f29e3928
usermwspace <user@mwspace.com>
2021-09-08 11:47:13 +0200

    Added relationship

--- app/Models/Apartment.php
+++ app/Models/Apartment.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Apartment extends Model
 {
     use HasFactory;
+
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+    public function messages()
+    {
+        return $this->hasMany(Message::class);
+    }
 }
