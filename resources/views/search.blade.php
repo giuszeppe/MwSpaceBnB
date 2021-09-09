74cfbad14021fb8c5312c7974fe769970e5059f2
usermwspace <user@mwspace.com>
2021-09-09 10:21:51 +0200

    migrate search bar from hardcoded to blade template

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -1,68 +1,8 @@
 @extends('layouts.app') @section('content')
-<section class="tg-sectionspace" style="padding-bottom:30px">
-    <div class="container">
-        <div class="row">
-            <form class="tg-formtheme tg-formtrip">
-                <fieldset class="search-form-wrapper">
-                    <div class="form-group">
-                        <div class="tg-input">
-                            <input
-                                type="text"
-                                name="address"
-                                class="tg-input-text"
-                                placeholder="Dove vuoi alloggiare?"
-                            />
-                        </div>
-                    </div>
-        
-                    <div class="form-group">
-                        <div class="tg-select">
-                            <select
-                                class="selectpicker"
-                                data-live-search="true"
-                                data-width="100%"
-                            >
-                                <option data-tokens="adul>ti">Adulti</option>
-                                <option data-tokens="2 persone">
-                                    2 persone
-                                </option>
-                                <option data-tokens="3 persone">
-                                    3 persone
-                                </option>
-                                <option data-tokens="4 persone">
-                                    4 persone
-                                </option>
-                                <option data-tokens="5 persone">
-                                    5 persone
-                                </option>
-                                <option data-tokens="6 persone">
-                                    6 persone
-                                </option>
-                                <option data-tokens="7 persone">
-                                    7 persone
-                                </option>
-                                <option data-tokens="8 persone">
-                                    8 persone
-                                </option>
-                                <option data-tokens="9 persone">
-                                    9 persone
-                                </option>
-                                <option data-tokens="10 persone">
-                                    10 persone
-                                </option>
-                            </select>
-                        </div>
-                    </div>
-                    <div id="tg-search-submit">
-                        <button class="tg-btn" type="submit">
-                            <span>find tours</span>
-                        </button>
-                    </div>
-                </fieldset>
-            </form>
-        </div>
-    </div>
-</section>
+
+
+<x-navbar> </x-navbar>
+
 
 <section class="tg-sectionspace tg-haslayout" style="padding-top:30px;">
     <div class="container-fluid" id="myContainer">
