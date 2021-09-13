e61cd6927f1614d948caf9754b64048bcdd723ee
usermwspace <user@mwspace.com>
2021-09-13 10:05:37 +0200

    Added properly the servizi aggiuntivi input field

--- public/js/additional_services.js
+++ public/js/additional_services.js
@@ -1,39 +1,42 @@
-document.getElementById('add_btn').onclick = function(){
-	event.preventDefault();
-	var count = $('#servizi_aggiuntivi > input').length;
-
-	let list = document.getElementById('servizi_aggiuntivi');
-	let node = document.createElement('INPUT');
+buildElement = function (count,text) {
 	let wrapper = document.createElement('span');
+	wrapper.setAttribute('class','span-services');
+	let list = document.getElementById('servizi_aggiuntivi');
+	let textnode = document.createTextNode(text);
+	let node = document.createElement('INPUT');
 	let delete_btn = document.createElement('button');
 	let list_item = document.createElement('Li');
-
-
-
-	let text = document.getElementById('servizio').value;
-	let textnode = document.createTextNode(text);
-
 	
 	list_item.textContent = text;
+
 	wrapper.setAttribute('id', 'servizio' + count);
 
 	delete_btn.setAttribute('name','servizio' + count);
+	delete_btn.setAttribute('class','btn btn-danger btn-sm mt-2 mb-2 ml-3');
 	delete_btn.innerHTML = '<i class="fa fa-times" aria-hidden="true"></i>';
 
 	delete_btn.onclick= function () {
 		event.preventDefault();
+		let count = 0;
+		let list = document.getElementById('servizi_aggiuntivi')
 		let name = delete_btn.name;
-		let elems = document.getElementById('servizi_aggiuntivi').children;
+		let elems = list.cloneNode(true).children;
 		console.log(elems)
-		for(elems of elem){
+		list.innerHTML = "";
 
+		console.log(elems)
+		for(elem of elems){
+			if(elem.id == name){
+				continue;
+			} else {
+				buildElement(count,elem.children[0].textContent);
+				count++;
+			}
 		}
-
 	}
 
 	wrapper.appendChild(list_item);
 	wrapper.appendChild(delete_btn);
-
 
 
 	node.setAttribute('type','checkbox');
@@ -41,8 +44,14 @@ document.getElementById('add_btn').onclick = function(){
 	node.setAttribute('style','display:none');
 	node.appendChild(textnode);
 
-	
-
 	wrapper.appendChild(node);
 	list.append(wrapper);
+} 
+
+document.getElementById('add_btn').onclick = function(){
+	event.preventDefault();
+	var count = $('#servizi_aggiuntivi > span').length;
+	let text = document.getElementById('servizio').value;
+	buildElement(count, text);
+
 }
