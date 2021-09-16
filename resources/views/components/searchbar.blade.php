1798ad62acf135bde9f4d8918662763e3edcc4fa
usermwspace <user@mwspace.com>
2021-09-16 10:40:15 +0200

    Deleted select option for the number of people, redudant choice

--- resources/views/components/searchbar.blade.php
+++ resources/views/components/searchbar.blade.php
@@ -1,56 +1,17 @@
 <section class="tg-sectionspace" style="padding-bottom:30px">
     <div class="container">
         <div class="row">
-            <form class="tg-formtheme tg-formtrip">
+            <form class="tg-formtheme tg-formtrip" id="searchForm" action="{{route('postSearch')}}" method="POST">
+                @csrf
                 <fieldset class="search-form-wrapper">
                     <div class="form-group">
                         <div class="tg-input">
                             <input
                                 type="text"
-                                name="address"
+                                name="title"
                                 class="tg-input-text"
                                 placeholder="Dove vuoi alloggiare?"
                             />
-                        </div>
-                    </div>
-        
-                    <div class="form-group">
-                        <div class="tg-select" >
-                            <select
-                                class="selectpicker"
-                                data-live-search="true"
-                                data-width="100%"
-                                name="person_count"
-                            >
-                                <option data-tokens="adul>ti">Adulti</option>
-                                <option data-tokens="2 persone">
-                                    2 persone
-                                </option>
-                                <option data-tokens="3 persone">
-                                    3 persone
-                                </option>
-                                <option data-tokens="4 persone">
-                                    4 persone
-                                </option>
-                                <option data-tokens="5 persone">
-                                    5 persone
-                                </option>
-                                <option data-tokens="6 persone">
-                                    6 persone
-                                </option>
-                                <option data-tokens="7 persone">
-                                    7 persone
-                                </option>
-                                <option data-tokens="8 persone">
-                                    8 persone
-                                </option>
-                                <option data-tokens="9 persone">
-                                    9 persone
-                                </option>
-                                <option data-tokens="10 persone">
-                                    10 persone
-                                </option>
-                            </select>
                         </div>
                     </div>
                     <div id="tg-search-submit">
