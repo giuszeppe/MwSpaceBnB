cda30edcc9fc203cd972417a0b921a5af8986d0d
giuseppe <peppex.2502@gmail.com>
2021-09-19 22:57:45 +0200

    .

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -144,6 +144,5 @@
  <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"
    integrity="sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA=="
    crossorigin=""></script>
-   <script src="/js/map.js" View: /home/giuseppe/Documenti/GitHub Repo/MwSpaceBnB/resources/views/components/address-input.blade.php)
-   http://127.0.0.1:8000/ ></script>
+   <script src="/js/map.js"></script>
 @endsection
